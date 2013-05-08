6494d0b6d37b6bd1d11ba3159a8508677824032b
Brecht <b.deconinck@telenet.be>
2013-05-08 21:32:46 +0200

    Preprocessor class contains write and read of tweets

---
+++ Preprocessor.py
@@ -0,0 +1,36 @@
+'''
+Created on 8-mei-2013
+
+@author: Brecht Deconinck
+'''
+def readTwitterData(sFile):
+    tweets = []
+    try:
+        print "Read Data"
+        f = open(sFile, "r")
+        data = f.readlines()
+        for line in data:
+            #strip line from \n and \t
+            row = line.strip().split("\t")
+            #add row to tweets
+            tweets.append([row[0],row[1],row[2]])
+        print "Read Complete"
+    except IOError:
+        print 'Error: reading tweets'
+    finally:
+        f.close()
+    return tweets
+    
+def writeTwitterData(sFile, arr):
+    try:
+        print "Write Start"
+        f = open(sFile, "w")
+        for i in range(0, len(arr)):
+            #datum, user, text
+            row = arr[i][0] + "\t" + arr[i][1] + "\t" + arr[i][2] + "\n"
+            f.write(row)
+        print "Write Complete"
+    except IOError:
+        print 'Error: writing tweets'
+    finally:
+        f.close()
