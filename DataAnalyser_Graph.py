f8bdfa1373a156534be57d4b97d741b60157fa5a
Floris Cockaerts <ff_floris@hotmail.com>
2013-05-11 17:57:23 +0200

    small update on tweet counter

--- DataAnalyser_Graph.py
+++ DataAnalyser_Graph.py
@@ -7,12 +7,18 @@ import IO
 from time import strptime
 
 twitterFile = open("data/scrapeTest2.txt")
+my_dict = dict()
 for tweet in IO.readData_by_line(twitterFile):
     parts = tweet.strip().split('\t')
     time = strptime(parts[1][5:-6], "%d %B %Y %H:%M:%S")
     """02 May 2013 19:14:07
     time.struct_time(tm_year=2013, tm_mon=5, tm_mday=2, tm_hour=19, tm_min=14, tm_sec=7, tm_wday=3, tm_yday=122, tm_isdst=-1)
     """
-    print time
-    break
+    tag = str(time[0]) + "-" + str(time[1]) + "-" + str(time[2]) + "_" + str(time[3])
+    if (tag in my_dict):
+        my_dict[tag] += 1
+    else:
+        my_dict[tag] = 1
+        
+print my_dict
 twitterFile.close()
