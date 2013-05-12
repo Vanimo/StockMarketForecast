e3256d5aee3566fdabbeae2d473774c3b1f43144
Kevin <kevinvdg@gmail.com>
2013-05-12 17:00:48 +0200

    Small update and 2 new text files conating stopwords and emotions.

--- TweetReader.py
+++ TweetReader.py
@@ -17,6 +17,33 @@ class TweatReader:
             if len(split_tabs)>=4:
                 list_of_tweets.append(Tweet(split_tabs[0], split_tabs[3],_date=split_tabs[1][:-6],_user=split_tabs[2]))
         return list_of_tweets
+    
+    @staticmethod
+    def preprocessTweets(list_of_tweets):
+        stopWords = getStopWords("StopWords.txt")
+        goodFeelings, badFeelings = getEmotionWords("Emotions.txt")
+        
+    @staticmethod
+    def getStopWords(link): #De stopwoorden uit het document halen.
+        SWs = []
+        f = open(link, 'rU')
+        for line in f:
+            SWs.append(line.rstrip('\n'))
+            return SWs
+        
+    @staticmethod
+    def getEmotionWords(link): #De stopwoorden uit het document halen.
+        badFeelings = []
+        goodFeelings = []  
+        f = open(link, 'rU')  
+        for line in f:
+            split_tabs=line.split('\t')
+            if len(split_tabs)>=2:
+                if split_tabs[1]==1:
+                    goodFeelings.append(split_tabs[0])
+                else:
+                    badFeelings.append(split_tabs[0])
+        return goodFeelings, badFeelings
 
 class Tweet:
     def __init__(self, _id, _tweet,_date="Mon, 01 January 1900 00:00:00", _user="",_label=0):
